42293cfe3e0fcc2a61be4aba6405c9a8b8e7df2a
uday <udaykiranvalaboju@gmail.com>
2023-11-24 00:16:27 +0530

    grid photos

--- src/components/Practices/index.js
+++ src/components/Practices/index.js
@@ -4,7 +4,7 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-
+import './Practices.css'
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
@@ -15,25 +15,25 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export function PracticeGrid() {
   return (
-    <Box sx={{ flexGrow: 1, padding: "32px" }}>
+    <Box sx={{ flexGrow: 1, padding: "0px 6rem 0px 6rem" }}>
       <Grid container spacing={2}>
         <Grid item xs={9} className='item1'>
-          <img src="Rectangle 3652.png" alt="no-img"></img>
+          <img src="Rectangle 3652.png" alt="no-img" className='practice-img'></img>
         </Grid>
         <Grid item xs={3} className='item2'>
-          <img src="Rectangle 3651.png" alt="no-img"></img>
+          <img src="Rectangle 3651.png" alt="no-img" className='practice-img'></img>
         </Grid>
         <Grid item xs={3} className='item2'>
-          <img src="Rectangle 3653.png" alt="no-img"></img>
+          <img src="Rectangle 3653.png" alt="no-img" className='practice-img'></img>
         </Grid>
         <Grid item xs={9} className='item1'>
-          <img src="Rectangle 3654.png" alt="no-img"></img>
+          <img src="Rectangle 3654.png" alt="no-img" className='practice-img'></img>
         </Grid>
         <Grid item xs={3} className='item2'>
-          <img src="Rectangle 3656.png" alt="no-img"></img>
+          <img src="Rectangle 3656.png" alt="no-img" className='practice-img'></img>
         </Grid>
         <Grid item xs={9} className='item1'>
-          <img src="Rectangle 3655.png" alt="no-img"></img>
+          <img src="Rectangle 3655.png" alt="no-img" className='practice-img'></img>
         </Grid>
       </Grid>
     </Box>
