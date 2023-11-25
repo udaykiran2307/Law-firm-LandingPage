3da7a147afae7a8992cdb7a06fa83c8b562815ef
uday <udaykiranvalaboju@gmail.com>
2023-11-25 15:11:52 +0530

    intro css

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,29 +1,14 @@
 /* eslint-disable import/no-anonymous-default-export */
 
+import './intro.css'
 export default () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        padding:'8rem',
-        marginLeft:'8rem'
-      }}
-    >
-      <h1 style={{maxWidth:'15rem'}}>Let's Introduce Ourself</h1>
-      <div
-        style={{
-          height: "206px",
-          border: "1px solid #434343",
-          marginRight: "2rem",
-          marginLeft:'2rem'
-          
-        }}
-      ></div>
-      <div style={{minWidth: "200px", maxHeight:'300px'}}>
+    <div className="intro-section">
+      <h1 className="intro-head">Let's Introduce Ourself</h1>
+      <div className="line"></div>
+      <div className="into-text">
         <h3>Criminal Lawyer</h3>
-        <p style={{ color: "#616161", minWidth: "20vw", maxHeight:'300px', marginTop:'1rem'}}>
+        <p className="intro-sub-text">
           Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
           sint. Velit officia consequatduis enim velit mollit Exercitation.
         </p>
